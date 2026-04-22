bbbcbb4ef0a042c02d5977f3261c356e41914b21
Ankit-84 <ankitkumar43975@gmail.com>
2026-04-23 00:36:19 +0530

    streamlit + home screen

---
+++ src/components/footer.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def footer_home():
+
+    st.markdown("""
+                
+            <div style="margin-top:2rem; display:flex;  gap:6px;  align-items:center; justify-content:center;" >
+                <p style="font-weight:bold; color:white;">Created with ❤️ by Ankit Kumar</p> 
+            </div>
+                
+                
+                """,unsafe_allow_html=True)
