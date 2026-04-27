8401b6d5a0afec0efefcb9df923a0b549c3075fe
Ankit-84 <ankitkumar43975@gmail.com>
2026-04-28 00:12:46 +0530

    done

--- src/components/footer.py
+++ src/components/footer.py
@@ -10,3 +10,14 @@ def footer_home():
                 
                 
                 """,unsafe_allow_html=True)
+    
+def footer_dashboard():
+
+    st.markdown("""
+                
+            <div style="margin-top:2rem; display:flex;  gap:6px;  align-items:center; justify-content:center;" >
+                <p style="font-weight:bold; color:black;">Created with ❤️ by Ankit Kumar</p> 
+            </div>
+                
+                
+                """,unsafe_allow_html=True)
